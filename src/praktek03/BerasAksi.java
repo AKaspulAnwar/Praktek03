4f6650af45eb31f915025296183602e1480df4a9
AKaspulAnwar <32286210+AKaspulAnwar@users.noreply.github.com>
2018-04-08 11:26:01 +0800

    instance,buat atribut dan panggil method

--- src/praktek03/BerasAksi.java
+++ src/praktek03/BerasAksi.java
@@ -1,6 +1,13 @@
 package praktek03;
 
 public class BerasAksi {
-    
+    public static void main(String[] args) {
+        Beras Siam = new Beras();
+        Siam.warna = "Putih Bening";
+        Siam.hasil = "Karau";
+        Siam.harga = 10000;
+        
+        Siam.cetakInfo();
+    }
     
 }
