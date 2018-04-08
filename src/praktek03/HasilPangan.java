22c6a3939d1507f1ed4727abd68071a4a7323104
AKaspulAnwar <32286210+AKaspulAnwar@users.noreply.github.com>
2018-04-08 11:49:49 +0800

    Getter and Setter

--- src/praktek03/HasilPangan.java
+++ src/praktek03/HasilPangan.java
@@ -5,6 +5,14 @@ public class HasilPangan extends Beras{
     
     void throttle(){
         System.out.println("Padi");
+    } 
+
+    public String getJenisPangan() {
+        return jenisPangan;
+    }
+
+    public void setJenisPangan(String jenisPangan) {
+        this.jenisPangan = jenisPangan;
     }
     
 }
