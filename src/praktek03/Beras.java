edfd62edce044250637474787a227e236e12402d
AKaspulAnwar <32286210+AKaspulAnwar@users.noreply.github.com>
2018-04-08 11:15:33 +0800

    Membuat Method cetakInfo()

--- src/praktek03/Beras.java
+++ src/praktek03/Beras.java
@@ -4,4 +4,9 @@ public class Beras {
     String warna;
     String hasil;
     double harga;
+    
+    void cetakInfo(){
+        System.out.println("Warna \t: "+warna+"\n"+"Hasil \t: "+hasil+"\n"+"Harga \t: "+harga);
+        
+    }
 }
